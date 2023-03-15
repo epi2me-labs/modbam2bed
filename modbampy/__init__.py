2ec4bed04fdb8f32d2e2164e5c4bd57ea6d72844
Chris Wright <chris.wright@nanoporetech.com>
2023-03-15 13:17:09 +0000

    Bump version

--- modbampy/__init__.py
+++ modbampy/__init__.py
@@ -8,7 +8,7 @@ import numpy as np
 import libmodbampy
 
 # remember to bump version in src/version.h too
-__version__ = "0.9.2"
+__version__ = "0.9.3"
 ffi = libmodbampy.ffi
 libbam = libmodbampy.lib
 
